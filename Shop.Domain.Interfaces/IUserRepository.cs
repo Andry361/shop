bf7db3c7bf879c590e81966c41ead54d0a63022f
Andry <andry361@yandex.ru>
2016-11-16 11:25:01 +0300

    Onion архитектура

---
+++ Shop.Domain.Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Shop.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Interfaces
+{
+  interface IUserRepository
+  {
+    IEnumerable<User> GetUsers();
+    User GetUser(Guid id);
+    void Create(User user);
+    void Update(User user);
+    void Delete(Guid id);
+    void Save();
+  }
+}
---
+++ Shop.Services.Interfaces/IAccessControlManager.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Services.Interfaces
+{
+  interface IAccessControlManager
+  {
+    void Authorize(User user);
+  }
+}
